02a6076da14e3650633bf34de7407782700b98a7
GrangerVMV <javitrunk@hotmail.com>
2022-09-15 18:20:10 +0200

    Segundo ejemplo: Uso del Condicional If sin llaves

--- Program.cs
+++ Program.cs
@@ -15,8 +15,17 @@ namespace CursoCPractica3_Condicionales
             int edad = int.Parse(Console.ReadLine());
             if (edad >= 18) 
             {
-                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");                
             }
+
+
+            // Segundo ejemplo: Uso del Condicional If sin llaves
+
+            Console.WriteLine("Por favor, indica si tienes reserva (Yes/No)");
+            string reserva = Console.ReadLine();
+            if (reserva == "Yes") // Si el If solo tiene 1 lin de código no hacen falta llaves
+                Console.WriteLine("Adelante, puedes pasar porque tienes reserva"); // Esta línea siempre va a pertenecer al If y la lee o no la lee dependiendo de si cumple la condición
+                Console.WriteLine("Disfruta de tu estacia"); // Esta línea no pertenece al If y por lo tanto el flujo de ejecución la lee siempre    
         }
     }
 }
