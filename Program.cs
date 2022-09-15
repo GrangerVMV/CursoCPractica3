c1ca3d3b9458ded7912442e2d81d67829230fe64
GrangerVMV <javitrunk@hotmail.com>
2022-09-15 17:11:07 +0200

    Negamos el valor de una variable booleana con operador !

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CursoCPractica3_Condicionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool haceFrio = false;
+            Console.WriteLine($"El valor de la variable booleana <haceFrio> negada (!) es {!haceFrio}");
+        }
+    }
+}
