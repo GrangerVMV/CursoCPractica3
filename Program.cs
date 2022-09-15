e7523408534fca7d4e9909c8e7112f5a67f1ac80
GrangerVMV <javitrunk@hotmail.com>
2022-09-15 17:24:34 +0200

    Primer ejemplo: Uso del Condicional If

--- Program.cs
+++ Program.cs
@@ -7,7 +7,16 @@ namespace CursoCPractica3_Condicionales
         static void Main(string[] args)
         {
             bool haceFrio = false;
-            Console.WriteLine($"El valor de la variable booleana <haceFrio> negada (!) es {!haceFrio}");
+            Console.WriteLine($"El valor de la variable booleana <haceFrio> negada (!) es {!haceFrio}"); // usamos el operador ! para negar el valor de una variable 
+
+            // Primer ejemplo: Uso del Condicional If
+
+            Console.WriteLine("Por favor, introduce tu edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18) 
+            {
+                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+            }
         }
     }
 }
