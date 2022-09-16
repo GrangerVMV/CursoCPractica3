b16fb2eb5dcb70d62f609d3b4a5be6b6857accce
GrangerVMV <javitrunk@hotmail.com>
2022-09-16 18:39:20 +0200

    Cuarto ejemplo: Uso del Condicional If/Else con operador lógico &&

--- Program.cs
+++ Program.cs
@@ -35,6 +35,16 @@ namespace CursoCPractica3_Condicionales
             if (carnet) Console.WriteLine("Enhorabuena, puedes conducir vehículos");// Entra si la variables es true, no es necesario poner carnet==true
             else Console.WriteLine("Lo siento, no puedes conducir vehículos");// Entra si la condición del if no se cumple. Si tuviera más de una línea habría que poner llaves
 
+
+            // Cuarto ejemplo: Uso del Condicional If/Else con operador lógico &&
+            Console.WriteLine("Vamos a evaluar si puedes conducir un vehiculo");
+            Console.WriteLine("Por favor, introduce tu edad");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indique si tiene carnet de conducir (Yes/No)");
+            string carnet2 = Console.ReadLine();
+            if (carnet2 == "Yes" && edad >= 18) Console.WriteLine("Enhorabuena, puedes conducir vehiculos");
+            else Console.WriteLine("Lo siento no puedes conducir vehiculos");
+            
         }
     }
 }
